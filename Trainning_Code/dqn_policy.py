325500cf36a36a1e8db0cc503e4b0d7780d291ba
Shohdi ElSheemy <shohdi@gmail.com>
2022-09-19 22:07:44 +0300

    fix sample in exp buffer

--- Trainning_Code/dqn_policy.py
+++ Trainning_Code/dqn_policy.py
@@ -145,8 +145,8 @@ class ExperienceBuffer:
     def sample(self, batch_size):
         indices = np.random.choice(len(self.buffer), batch_size, replace=False)
         states, actions, rewards, dones, next_states = zip(*[self.buffer[idx] for idx in indices])
-        return np.array(states), np.array(actions), np.array(rewards, dtype=np.float32), \
-               np.array(dones, dtype=np.uint8), np.array(next_states)
+        return np.array(states,dtype=np.float32), np.array(actions,dtype=np.int64), np.array(rewards, dtype=np.float32), \
+               np.array(dones, dtype=np.uint8), np.array(next_states,dtype=np.float32)
     
     
 
