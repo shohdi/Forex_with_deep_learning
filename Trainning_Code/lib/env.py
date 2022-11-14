5cbe4785ea2c75313c8c84c037a92e2e4d4c8bc5
Shohdy ElSheemy <shohdi@gmail.com>
2022-11-14 22:53:42 +0300

    finish punish agent in case wait without trading to -0.01 , tests all success

--- Trainning_Code/lib/env.py
+++ Trainning_Code/lib/env.py
@@ -111,7 +111,7 @@ class ForexEnv(gym.Env):
         
         if self.startTradeStep is None:
             if self.stepIndex > 200 and self.punishAgent:
-                reward = 0
+                reward = -0.01
                 done = True
         return state , reward , done ,None
 
