ec773890803bbbc420c3b0d5b86761125c92d501
Shohdy ElSheemy <shohdi@gmail.com>
2022-11-27 20:34:05 +0300

    make it full network

--- Trainning_Code/dqn_rainbow.py
+++ Trainning_Code/dqn_rainbow.py
@@ -103,11 +103,17 @@ class LSTM_Forex (nn.Module):
         self.actions = actions
         self.selected_device = selDevice
         self.inSize = self.input_shape[1]
-        self.hiddenSize = 200
+        self.hiddenSize = 1024
         self.numLayers = 2
         self.outSize = 512
-        self.lstm = nn.LSTM(self.inSize,self.hiddenSize,self.numLayers,batch_first=True)
-        
+        #self.lstm = nn.LSTM(self.inSize,self.hiddenSize,self.numLayers,batch_first=True)
+        self.size = np.prod(self.input_shape)
+        self.network = nn.Sequential(
+            nn.Linear(self.size, self.hiddenSize),
+            nn.ReLU(),
+            nn.Linear(self.hiddenSize, self.hiddenSize),
+            nn.ReLU()
+        )
         
 
 
@@ -128,11 +134,15 @@ class LSTM_Forex (nn.Module):
     
     def forward(self,x):
         batch_size = x.size()[0]
-        h0 = torch.zeros(self.numLayers,x.size(0),self.hiddenSize,device=self.selected_device)
-        c0 = torch.zeros(self.numLayers,x.size(0),self.hiddenSize,device=self.selected_device)
-        out,(hn,cn) = self.lstm(x,(h0,c0))
-        val_out = self.fc_val(out[:,-1,:]).view(batch_size, 1, N_ATOMS)
-        adv_out = self.fc_adv(out[:,-1,:]).view(batch_size, -1, N_ATOMS)
+        #h0 = torch.zeros(self.numLayers,x.size(0),self.hiddenSize,device=self.selected_device)
+        #c0 = torch.zeros(self.numLayers,x.size(0),self.hiddenSize,device=self.selected_device)
+        #out,(hn,cn) = self.lstm(x,(h0,c0))
+        out = x.view(batch_size,-1)
+        out = self.network(out)
+        #val_out = self.fc_val(out[:,-1,:]).view(batch_size, 1, N_ATOMS)
+        #adv_out = self.fc_adv(out[:,-1,:]).view(batch_size, -1, N_ATOMS)
+        val_out = self.fc_val(out).view(batch_size, 1, N_ATOMS)
+        adv_out = self.fc_adv(out).view(batch_size, -1, N_ATOMS)
         adv_mean = adv_out.mean(dim=1, keepdim=True)
         return val_out + (adv_out - adv_mean)
     
