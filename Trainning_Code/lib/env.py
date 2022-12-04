d599ac1298caa4d9c3712ec28451d90575317a53
Shohdi ElSheemy <shohdi@gmail.com>
2022-12-04 20:58:56 +0300

    finish env data by variable length and test and val only one dir

--- Trainning_Code/lib/env.py
+++ Trainning_Code/lib/env.py
@@ -8,7 +8,8 @@ import csv
 import time
 
 class ForexEnv(gym.Env):
-    def __init__(self,filePath , punishAgent = True,stopTrade = True):
+    def __init__(self,filePath , haveOppsiteData:bool , punishAgent = True,stopTrade = True):
+        self.haveOppsiteData = haveOppsiteData
         self.punishAgent = punishAgent
         self.stopTrade = stopTrade
         self.filePath = filePath
@@ -33,16 +34,17 @@ class ForexEnv(gym.Env):
             reader = csv.reader(f, delimiter=';')
             self.header = next(reader)
             self.data_arr.append( np.array(list(reader)).astype(np.float32))
-            self.data_arr.append(np.array(self.data_arr[0],copy=True))
-            self.data_arr[1] = 1/self.data_arr[1]
-            tempData = np.array(self.data_arr[1][:,4],copy=True)
-            self.data_arr[1][:,4] = np.array(self.data_arr[1][:,5],copy=True)
-            self.data_arr[1][:,5] = tempData
-            tempData = np.array(self.data_arr[1][:,2],copy=True)
-            self.data_arr[1][:,2] = np.array(self.data_arr[1][:,3],copy=True)
-            self.data_arr[1][:,3] = tempData
+            if self.haveOppsiteData:
+                self.data_arr.append(np.array(self.data_arr[0],copy=True))
+                self.data_arr[1] = 1/self.data_arr[1]
+                tempData = np.array(self.data_arr[1][:,4],copy=True)
+                self.data_arr[1][:,4] = np.array(self.data_arr[1][:,5],copy=True)
+                self.data_arr[1][:,5] = tempData
+                tempData = np.array(self.data_arr[1][:,2],copy=True)
+                self.data_arr[1][:,2] = np.array(self.data_arr[1][:,3],copy=True)
+                self.data_arr[1][:,3] = tempData
 
-            self.data = self.data_arr[np.random.randint(2)]
+            self.data = self.data_arr[np.random.randint(len(self.data_arr))]
         
         test_state = self.reset()
         self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=test_state.shape, dtype=np.float32)
@@ -52,7 +54,7 @@ class ForexEnv(gym.Env):
     def reset(self):
         self.lastTenData.append((self.startIndex,self.startTradeStep,self.startClose,self.startAsk,self.startBid,self.openTradeDir))
         #print(self.lastTenData[-1])
-        self.data = self.data_arr[np.random.randint(2)]
+        self.data = self.data_arr[np.random.randint(len(self.data_arr))]
         self.startIndex = np.random.randint(len(self.data)-500)
         self.startTradeStep = None
         self.stepIndex = 0
@@ -289,7 +291,7 @@ if __name__ == "__main__":
 
 
 if __name__ == "__main__":
-    env = ForexEnv("minutes15_100/data/train_data.csv")
+    env = ForexEnv("minutes15_100/data/train_data.csv",True)
     state = env.reset()
     print("start " , state[0])
     print("start close ",env.startClose)
