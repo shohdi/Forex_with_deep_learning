68e6851d3b3a3d9efa60e7add900e3fe0dc0a0c1
Shohdy ElSheemy <shohdi@gmail.com>
2023-07-21 15:50:12 +0300

    decrease memory use by using list not dequeue

--- Trainning_Code/dqn_rainbow.py
+++ Trainning_Code/dqn_rainbow.py
@@ -224,10 +224,10 @@ def calc_loss(batch, batch_weights, net, tgt_net, gamma, device="cpu"):
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     startTime = time.time()
-    testRewards = collections.deque(maxlen=213)
+    testRewards = []#collections.deque(maxlen=213)
     
     testRewardsMean = 0
-    valRewards = collections.deque(maxlen=213)
+    valRewards = []#collections.deque(maxlen=213)
     valRewardsMean = 0
     params = common.HYPERPARAMS['Forex']
     params['epsilon_frames'] *= 2
@@ -342,6 +342,7 @@ if __name__ == "__main__":
                 if frame_idx % 100000 == 0:
                     
                     testIdx = 0
+                    testRewards=[]
                     while testIdx < 213:
                         testState = envTest.reset()
                         testState = np.array(testState,dtype=np.float32)
@@ -382,6 +383,7 @@ if __name__ == "__main__":
 
                     
                     valIndx = 0
+                    valRewards=[]
                     while valIndx < 213:
                         valState = envVal.reset()
                     
