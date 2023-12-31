4cd37311364b229f0aebd45180235967b065c209
Shohdy ELSheemy <shohdi@gmail.com>
2023-12-31 23:19:09 +0300

    fix env to not stop until sl come

--- Trainning_Code/lib/env.py
+++ Trainning_Code/lib/env.py
@@ -115,26 +115,26 @@ class ForexEnv(gym.Env):
 
     def step(self,action_idx):
         #check punish
-        
+        '''
         if self.openTradeDir == 1 and (self.stepIndex - self.startTradeStep) > (100 * 10) and self.stopTrade:
             action_idx = 2
         elif self.openTradeDir == 2 and (self.stepIndex - self.startTradeStep) > (100 * 10) and self.stopTrade:
             action_idx = 1
-        
-
         '''
+
+        stopLossPerc = 0.07
         if self.stopTrade:
             if self.openTradeDir == 1  :
-                reward = self.closeUpTrade() * 2.0
-                if reward > 0.0856 or reward < -0.0856:
+                reward = self.closeUpTrade()
+                if abs(reward * 2.0) >= stopLossPerc:
                     action_idx = 2
                     #print('stop trade!')
             elif self.openTradeDir == 2 :
-                reward = self.closeDownTrade() * 2.0
-                if reward > 0.0856 or reward < -0.0856:
+                reward = self.closeDownTrade()
+                if abs(reward * 2.0) >= stopLossPerc:
                     action_idx = 1
                     #print('stop trade!')
-        '''
+        
         #end of punish action
 
         #punish no action
